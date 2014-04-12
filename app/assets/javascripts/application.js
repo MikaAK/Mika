67979114962d91f98955e549c1b9b59f40690898
Mika <mikakalathil@gmail.com>
2014-04-12 07:00:25 -0700

    Add Homepage Animation --Still buggy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,11 +38,14 @@ function resize_section() {
 
 function button_down(element) {
   $element = $(element);
-  $element.hover(function () {
-    $(this).prev().addClass('button-down');
-  }, function () {
-    $(this).prev().removeClass('button-down');
-  });;
+  $element.hover(
+    function () {
+      $(this).prev().addClass('button-down');
+    },
+    function () {
+      $(this).prev().removeClass('button-down');
+    }
+  );
 }
 
 function triangle_animation() {
@@ -50,44 +53,74 @@ function triangle_animation() {
   $secret = $('.secret');
   $favorite = $('.favorite');
 
-  if ($window.width() < 992) {
-
-  }
-
-  $currentProject.hover(function() {
-    $(this).css('-webkit-animation-play-state', 'paused');
-    $secret.css('-webkit-animation-play-state', 'paused');
-    $favorite.css('-webkit-animation-play-state', 'paused');
-  }, function() {
-    $(this).css('-webkit-animation-play-state', 'running');
-    $secret.css('-webkit-animation-play-state', 'running');
-    $favorite.css('-webkit-animation-play-state', 'running');
-  });
-  $secret.hover(function() {
-    $currentProject.css('-webkit-animation-play-state', 'paused');
-    $favorite.css('-webkit-animation-play-state', 'paused');
-    $(this).css('-webkit-animation-play-state', 'paused');
-  }, function() {
-    $(this).css('-webkit-animation-play-state', 'running');
-    $currentProject.css('-webkit-animation-play-state', 'running');
-    $favorite.css('-webkit-animation-play-state', 'running');
-  });
-  $favorite.hover(function() {
-    $(this).css('-webkit-animation-play-state', 'paused');
-    $secret.css('-webkit-animation-play-state', 'paused');
-    $currentProject.css('-webkit-animation-play-state', 'paused');
-  }, function() {
-    $(this).css('-webkit-animation-play-state', 'running');
-    $secret.css('-webkit-animation-play-state', 'running');
-    $currentProject.css('-webkit-animation-play-state', 'running');
-  });
+  $currentProject.hover(
+    function() {
+      $(this).css('-webkit-animation-play-state', 'paused')
+      $secret.css('-webkit-animation-play-state', 'paused');
+      $favorite.css('-webkit-animation-play-state', 'paused');
+    },
+    function() {
+      $(this).css('-webkit-animation-play-state', 'running');
+      $secret.css('-webkit-animation-play-state', 'running');
+      $favorite.css('-webkit-animation-play-state', 'running');
+    }
+  );
+  $secret.hover(
+    function() {
+      $currentProject.css('-webkit-animation-play-state', 'paused');
+      $favorite.css('-webkit-animation-play-state', 'paused');
+      $(this).css('-webkit-animation-play-state', 'paused');
+    },
+    function() {
+      $(this).css('-webkit-animation-play-state', 'running');
+      $currentProject.css('-webkit-animation-play-state', 'running');
+      $favorite.css('-webkit-animation-play-state', 'running');
+    }
+  );
+  $favorite.hover(
+    function() {
+      $(this).css('-webkit-animation-play-state', 'paused');
+      $secret.css('-webkit-animation-play-state', 'paused');
+      $currentProject.css('-webkit-animation-play-state', 'paused');
+    },
+    function() {
+      $(this).css('-webkit-animation-play-state', 'running');
+      $secret.css('-webkit-animation-play-state', 'running');
+      $currentProject.css('-webkit-animation-play-state', 'running');
+    }
+  );
 }
 
-// function hide_box() {
-//   $('tab-content').hide();
-
-//   if ($window.window)
-// }
+function hide_box() {
+  if($(window).width() > 768) {
+    $('.tab-content').hide(0,
+      function() {
+        $(this).prev().css('right', '29.337803855%');
+        $(this).prev().children().children().click(
+          function () {
+            $('.favorite').off('mouseenter mouseleave').css('-webkit-animation-play-state', 'running');
+            $('.secret').off('mouseenter mouseleave').css('-webkit-animation-play-state', 'running');
+            $('.current-projects').off('mouseenter mouseleave').css('-webkit-animation-play-state', 'running');
+            $('.tab-selection').animate({right: 0}, 3000).queue(
+              function() {
+                $('.tab-content').show(1000);
+              }
+            ).queue(
+              function() {
+                $('.favorite').on('mouseenter mouseleave');
+                $('.secret').on('mouseenter mouseleave');
+                $('.current-projects').on('mouseenter mouseleave');
+              }
+            );
+          }
+        );
+      }
+    );
+  }
+  else {
+    $('.tab-content').show();
+  }
+}
 /* Functions For Web End
 =========================== */
 
@@ -95,15 +128,17 @@ function triangle_animation() {
 /* Called after document Load
 ================================ */
 $(document).ready(function() {
-
   var $mainLogo = $('#main-logo');
+
+  hide_box();
   triangle_animation();
   wrap_element_link_mobile($mainLogo, '/');
   resize_section();
   button_down('.project-year + .darkgreen-button');
 
   $window.resize(function() {
-    wrap_element_link_mobile($mainLogo, '/');
     resize_section();
-    });
+    wrap_element_link_mobile($mainLogo, '/');
+    hide_box();
+  });
 });
