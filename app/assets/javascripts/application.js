cfe17a0337873a526c3985f7af375324aa0d9fd0
Mika <mikakalathil@gmail.com>
2014-04-12 02:56:09 -0700

    Redo tri buttons + continue annimations

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,6 +45,49 @@ function button_down(element) {
   });;
 }
 
+function triangle_animation() {
+  $currentProject = $('.current-projects');
+  $secret = $('.secret');
+  $favorite = $('.favorite');
+
+  if ($window.width() < 992) {
+
+  }
+
+  $currentProject.hover(function() {
+    $(this).css('-webkit-animation-play-state', 'paused');
+    $secret.css('-webkit-animation-play-state', 'paused');
+    $favorite.css('-webkit-animation-play-state', 'paused');
+  }, function() {
+    $(this).css('-webkit-animation-play-state', 'running');
+    $secret.css('-webkit-animation-play-state', 'running');
+    $favorite.css('-webkit-animation-play-state', 'running');
+  });
+  $secret.hover(function() {
+    $currentProject.css('-webkit-animation-play-state', 'paused');
+    $favorite.css('-webkit-animation-play-state', 'paused');
+    $(this).css('-webkit-animation-play-state', 'paused');
+  }, function() {
+    $(this).css('-webkit-animation-play-state', 'running');
+    $currentProject.css('-webkit-animation-play-state', 'running');
+    $favorite.css('-webkit-animation-play-state', 'running');
+  });
+  $favorite.hover(function() {
+    $(this).css('-webkit-animation-play-state', 'paused');
+    $secret.css('-webkit-animation-play-state', 'paused');
+    $currentProject.css('-webkit-animation-play-state', 'paused');
+  }, function() {
+    $(this).css('-webkit-animation-play-state', 'running');
+    $secret.css('-webkit-animation-play-state', 'running');
+    $currentProject.css('-webkit-animation-play-state', 'running');
+  });
+}
+
+// function hide_box() {
+//   $('tab-content').hide();
+
+//   if ($window.window)
+// }
 /* Functions For Web End
 =========================== */
 
@@ -54,7 +97,7 @@ function button_down(element) {
 $(document).ready(function() {
 
   var $mainLogo = $('#main-logo');
-
+  triangle_animation();
   wrap_element_link_mobile($mainLogo, '/');
   resize_section();
   button_down('.project-year + .darkgreen-button');
