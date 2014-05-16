481176732f051cbb99c25a9dfe9f1a89c4b00295
Mika <mikakalathil@gmail.com>
2014-05-16 13:58:02 -0700

    Quick Changes for Admin settings

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,9 @@
 class ProjectsController < ApplicationController
-
-
   http_basic_authenticate_with :name => ENV["WEBSITE_USERNAME"],
                                :password => ENV["WEBSITE_PASSWORD"],
-                               :only => ['edit', 'destroy', 'new'] if Rails.env.production?
+                               :only => ['edit', 'destroy', 'new']
+
+  before_action :index, :authenticate
 
   def index
     @projects = Project.all
@@ -27,6 +27,14 @@ class ProjectsController < ApplicationController
       redirect_to projects_path, notice: 'Project added successfully'
     else
       render 'new'
+    end
+  end
+  
+  protected
+
+  def authenticate
+    if request.subdomain == 'admin'
+      authenticate_or_request_with_http_basic("Administration") do |user,pass| user == ENV["WEBSITE_USERNAME"] && pass = ENV["WEBSITE_PASSWORD"] end 
     end
   end
 
